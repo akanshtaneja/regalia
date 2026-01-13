f0ddb5c231f01ffc02411db318152be3e7a98dbd
Akansh Taneja <akansh.taneja@publicissapient.com>
2026-01-13 10:56:13 +0530

    find size issue

--- src/Components/common/FindSize.jsx
+++ src/Components/common/FindSize.jsx
@@ -44,22 +44,22 @@ export const FindSize = ({
             transition={{ type: "spring", stiffness: 180, damping: 22 }}
           >
             {/* header */}
-            <div className="flex items-center justify-between pb-4 mb-4 mt-2">
-              <h2 className="text-xl font-bold tracking-wide uppercase">
+            <div className="flex items-center justify-between pb-4 mb-4 mt-2 md:pl-0 pl-5">
+              <h2 className="md:text-xl font-bold tracking-wide uppercase text:lg">
                 Choose a Size
               </h2>
               <button onClick={onClose}>
-                <IoClose className="text-2xl hover:text-black" />
+                <IoClose className="md:text-2xl hover:text-black text-xl"/>
               </button>
             </div>
 
             {/* size buttons */}
-            <div className="flex flex-col gap-3 mb-auto mx-auto w-[280px]">
+            <div className="flex flex-col gap-3 mb-auto mx-auto md:w-[280px] w-[240px]">
               {singleProduct?.sizes?.map((item, index) => (
                 <button
                   key={index}
                   onClick={() => setLocalSize(item)}
-                  className={` w-full h-14 text-sm font-semibold tracking-wide uppercase flex items-center px-5 transition hover:border-black
+                  className={` w-full md:h-14 h-12 text-sm font-semibold tracking-wide uppercase flex items-center px-5 transition hover:border-black
                     ${ localSize === item ? " border border-black" : "border border-gray-300"}`}>
                   {item}
                 </button>
@@ -69,11 +69,11 @@ export const FindSize = ({
                 <button
                   ref={confirmBtnRef}
                   onClick={() => onConfirm(localSize)}
-                  className="bg-black text-white uppercase py-4 px-3 text-md mt-5"
+                  className="bg-black text-white uppercase py-4 px-3 md:text-md mt-5 text-sm tracking-wider"
                 >
                   confirm your selection
                 </button>
-              )}
+              )} 
             </div>
           </motion.div>
         </>
